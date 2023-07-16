50e4a8f2a95c94f74467d50c1cc1e0bbf31da46a
Yusef-Mohamed <yoousefmohaamd@gmail.com>
2023-07-16 09:16:42 +0300

    landing vidoe

--- src/component/landing/Landing.jsx
+++ src/component/landing/Landing.jsx
@@ -1,109 +1,44 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import "./landing.css";
-import Particles from "react-particles";
-import { loadFull } from "tsparticles";
 const Landing = () => {
-  const particlesInit = useCallback(async (engine) => {
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
+  const vid = useRef();
+  const [url, setUrl] = useState("");
+  useEffect(() => {
+    fetch(`https://api.wealthmakers-fx.com/api/v1/landingPage`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) =>
+        data.data.map((e) => {
+          if (e.isDefault) {
+            setUrl(e.videoUrl);
+          }
+        })
+      );
   }, []);
-
-  const particlesLoaded = useCallback(async (container) => {}, []);
-
-  const canvas = document.querySelector(".landing canvas");
-  if (canvas) {
-    console.log((canvas.style = ""));
-  }
   return (
-    <div className="landing" style={{ position: "relative" }}>
-      <div
-        className="contain"
-        style={{ position: "absolute", width: "100%", height: "100%" }}
-      >
-        <Particles
-          id="tsparticles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={{
-            fpsLimit: 80,
-            interactivity: {
-              events: {
-                onClick: {
-                  enable: true,
-                  mode: "push",
-                },
-                onHover: {
-                  enable: true,
-                  mode: "grab",
-                },
-                resize: true,
-              },
-              modes: {
-                push: {
-                  quantity: 4,
-                },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4,
-                },
-                grab: {
-                  distance: 300,
-                  duration: 0.4,
-                },
-              },
-            },
-            particles: {
-              color: {
-                value: "#ffffff",
-              },
-              links: {
-                color: "#ffffff",
-                distance: 150,
-                enable: true,
-                opacity: 0.5,
-                width: 1,
-              },
-              collisions: {
-                enable: true,
-              },
-              move: {
-                direction: "none",
-                enable: true,
-                outModes: {
-                  default: "bounce",
-                },
-                random: false,
-                speed: 2,
-                straight: false,
-              },
-              number: {
-                density: {
-                  enable: true,
-                  area: 800,
-                },
-                value: 80,
-              },
-              opacity: {
-                value: 0.5,
-              },
-              shape: {
-                type: "circle",
-              },
-              size: {
-                value: { min: 1, max: 5 },
-              },
-            },
-            detectRetina: true,
-          }}
-        />
+    <div className="landing">
+      <div>
+        <video ref={vid} autoPlay muted loop src={url}></video>
       </div>
-
-      <div className="container" data-aos="fade-up" data-aos-duration="1000">
-        <h1>Live smarter </h1>
-        <p>Enhance your lifestyle.</p>
-        <button>Get Started</button>
+      <div
+        className="holder"
+        onMouseEnter={(e) => vid.current.pause()}
+        onMouseLeave={(e) => vid.current.play()}
+      >
+        <div
+          className="container"
+          data-aos="fade-up"
+          style={{ zIndex: "10" }}
+          data-aos-duration="1000"
+        >
+          <h1>Live smarter </h1>
+          <p>Enhance your lifestyle.</p>
+          <button>Get Started</button>
+        </div>
       </div>
     </div>
   );
