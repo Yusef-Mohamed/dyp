6526b002041efb8f68861a2a740a9ab6edb5d39b
Yusef-Mohamed <yoousefmohaamd@gmail.com>
2023-07-31 18:15:25 +0300

    0

--- src/component/Leadership/Leadership.jsx
+++ src/component/Leadership/Leadership.jsx
@@ -30,14 +30,6 @@ function Leadership() {
             }}
           />
         ))}
-
-        <LeaderCard
-          data={{
-            img: "https://igeniusglobal.com/static/media/codeChad.bd8f1cc0694568a2a54e.png",
-            name: "CHAD GARNER President",
-            des: "Chad brings over 18 years of sales, operations, and marketing experience. Chad was a founding member of the executive team for Daily Bread, a successful direct sales company that was sold to a private equity firm. He has built and scaled multiple high impact sales organizations throughout his career. Chad led the Inside Sales, Outside Sales, Affiliate Sales, Business Development, and Customer Success departments while helping raise venture capital for a technology startup called PeopleKeep. His passion is helping others improve their financial situation so they can live stress-free and enjoy life.            ",
-          }}
-        />
       </div>
       <div style={{ backgroundColor: "#ededed" }} className="p-5">
         <p className="container p-3 text-center">
